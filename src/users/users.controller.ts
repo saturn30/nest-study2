e4a5ce8f9e4bd17f14038a7d90041423558d6907
bluehead01 <main@bluehead.dev>
2023-06-25 16:45:06 +0900

    chapter3 완료

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateUesrDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfo } from './UserInfo';
+
+@Controller('users')
+export class UsersController {
+  @Post()
+  async createUser(@Body() dto: CreateUesrDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
+  }
+}
