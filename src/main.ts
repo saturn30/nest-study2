bd9c971dd2f0899164cf3a26581b7028e545ffc1
saturn30 <soso01dev@gmail.com>
2023-07-03 20:02:00 +0900

    13장 인터셉터

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { GlobalLoggerMiddleware } from './common/logger.middleware';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { HttpExceptionFilter } from './common/exception/httpException.filter';
+import { LoggingInterceptor } from './common/logging.interceptor';
+import { transformInterceptor } from './common/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new transformInterceptor(),
+  );
   app.use(GlobalLoggerMiddleware);
   await app.listen(3000);
 }
